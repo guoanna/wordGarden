966c84f288360f65887dfc271d8025462d717d54
Anna Guo <annaguo03@gmail.com>
2019-02-04 15:40:43 -0500

    3.2 Commit: - Changed textfield properties (only capital letter, centered) - Enabled keyboard & hide keyboard when pressed done - Connected new outlets and actions

--- wordGarden/ViewController.swift
+++ wordGarden/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessedLetterButton: UIButton!
+    @IBOutlet weak var guessLetterLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    func updateUIAfterGuess() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func guessButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+    }
+    
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+    }
+    
 }
 
